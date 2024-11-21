de744768f21cdc92eff50075404dab8298bedf0b
PC <dinahsisou@gmail.com>
2024-11-21 16:28:47 +0100

    supprimer inutiles imports

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,7 +7,6 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
-import { ProfileScreen } from './profile';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
