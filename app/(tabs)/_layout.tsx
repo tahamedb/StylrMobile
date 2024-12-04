d3c70bc9501932810b5edad4f79670d85f936fd2
PC <dinahsisou@gmail.com>
2024-12-04 19:12:47 +0100

    remove unused .env.example file, update API URL, and clean up package dependencies

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -48,7 +48,7 @@ export default function TabLayout() {
                 tabBarIcon: ({ color }) => <IconSymbol size={28} name="tshirt.fill" color={color} />,
               }}
             />*/}
-            />
+            
 
 
 
